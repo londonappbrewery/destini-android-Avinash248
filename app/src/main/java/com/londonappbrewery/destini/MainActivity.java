4f4a6071a65e48eab564db15f96a7caacd2f123a
Avinash Mishra <avi.born2win1998@gmail.com>
2017-06-24 12:32:41 +0530

    Project completed.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,18 @@
 package com.londonappbrewery.destini;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView story;
+    Button mTop;
+    Button mBottom;
+    int mIndex = 1;
 
 
     @Override
@@ -15,16 +22,97 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        story = (TextView) findViewById(R.id.storyTextView);
+        mTop = (Button) findViewById(R.id.buttonTop);
+        mBottom = (Button) findViewById(R.id.buttonBottom);
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
+            mTop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    updateTopButton(mIndex);
+
+                    if(mIndex == 1 || mIndex == 2)
+                    {
+                        mIndex = 3;
+                    }
+                    else if(mIndex == 3)
+                    {
+                        mIndex = 6;
+                    }
+
+                }
+            });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
 
+            mBottom.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    updateBottomButton(mIndex);
+                    if(mIndex == 1)
+                        mIndex = 2;
+                    else if(mIndex == 2)
+                        mIndex = 4;
+                    else if(mIndex == 3)
+                        mIndex = 5;
+
+                }
+            });
+
     }
+
+    public void updateTopButton(int mIndex)
+    {
+        if(mIndex == 1 || mIndex == 2)
+        {
+            story.setText(R.string.T3_Story);
+            mTop.setText(R.string.T3_Ans1);
+            mBottom.setText(R.string.T3_Ans2);
+        }
+        else if(mIndex == 3)
+        {
+            story.setText(R.string.T6_End);
+            mTop.setVisibility(View.INVISIBLE);
+            mBottom.setVisibility(View.INVISIBLE);
+
+        }
+
+    }
+
+    public void updateBottomButton(int mIndex)
+    {
+        if(mIndex == 1)
+        {
+            story.setText(R.string.T2_Story);
+            mTop.setText(R.string.T2_Ans1);
+            mBottom.setText(R.string.T2_Ans2);
+        }
+        else if(mIndex == 2) {
+            story.setText(R.string.T4_End);
+            mTop.setVisibility(View.INVISIBLE);
+            mBottom.setVisibility(View.INVISIBLE);
+
+        }
+        else
+            {
+                story.setText(R.string.T5_End);
+                mTop.setVisibility(View.INVISIBLE);
+                mBottom.setVisibility(View.INVISIBLE);
+            }
+
+    }
+
+
 }
+
+
+
